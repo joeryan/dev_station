598eeb011b9f46564ca0bf1b5e3b46d0c8934636
Joe Ryan <joe@clanryan.us>
2017-07-25 22:07:36 -0400

    add keyinfo to ansible recipe

--- cookbooks/ws_setup/recipes/ansible.rb
+++ cookbooks/ws_setup/recipes/ansible.rb
@@ -5,7 +5,10 @@ end
 apt_repository 'ansible' do
   uri	'ppa:ansible/ansible'
   components ['main', 'stable']
+  key '93C4A3FD7BB9C367'
 end
+
+apt_update
 
 package 'Install Ansible' do
   package_name 'ansible'
