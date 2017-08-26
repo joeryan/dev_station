7496362dedd6d9a8062578723f4ad4e948e4177d
Joe <joe@localhost.localdomain>
2017-08-25 22:40:30 -0400

    update py_dev.rb for centos/redhat

--- cookbooks/ws_setup/recipes/py_dev.rb
+++ cookbooks/ws_setup/recipes/py_dev.rb
@@ -1,3 +1,22 @@
-apt_package 'python3' do
+# Cookbook:: ws_setup
+# Recipe:: epel
+
+package 'Install epel-release for redhat/centos' do
+  package_name 'epel-release'
+  case node[:platform]
+    when 'redhat', 'centos'
+      action :install
+    when 'ubuntu', 'debian'
+      action :nothing
+    end
+end
+
+package 'Install python3' do
+  case node[:platform]
+    when 'redhat', 'centos'
+      package_name 'python34'
+    when 'ubuntu', 'debian'
+      package_name 'python3'
+    end
   action :install
 end
